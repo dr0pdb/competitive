c44385d162c6d2ed1d1d5706b62e64e136ac5fa8
srv-twry <saurav007tiwary@gmail.com>
2018-06-13 18:26:02 +0530

    solve a few more problems

---
+++ spoj/TWENDS.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+#define ABS(a) ((a < 0) ? ((-1)*(a)) : (a))
+#define F(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define RF(i,a,b) for(long long i = (long long)(a); i >= (long long)(b); i--)
+#define MIN3(a,b,c) (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c))
+#define MAX(a,b) (a)>(b)?(a):(b)
+#define MIN2(a,b) (a)<(b)?(a):(b)
+#define SIEVELIM 10000000+10
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+using namespace std;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef long long ll;
+#define ull unsigned long long
+typedef long double ld;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+inline int nextint(){ int temporary; scanf("%d",&temporary); return temporary; }
+inline ll nextll(){ ll temporary; scanf("%lld",&temporary); return temporary; }
+inline double nextdouble(){double temporary; scanf("%lf",&temporary); return temporary;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll leftChild(ll p ){return p<<1;}
+ll rightChild(ll p ){return (p<<1)+1;}
+inline ll mid(ll l, ll r){ return (l+r)/2;}
+const ll MOD = 1000000007;
+const ll INF = 1e9+5;
+const double eps = 1e-7;
+const double PI = acos(-1.0);
+#define deb(x )     cerr << #x << " here "<< x; 
+#define endl    "\n"
+#define pb push_back
+#define mp make_pair
+#define all(cc) (cc).begin(),(cc).end()
+
+inline bool is_palindrome(const string& s){ return std::equal(s.begin(), s.end(), s.rbegin()); }
+
+template<typename T > void modulize(T & a, const T & b) { if (a >= b) { a %= b; } }
+ll mulmod(ll a, ll b, ll m) { ll q = (ll)(((ld)a*(ld)b) / (ld)m); ll r = a*b - q*m; if (r>m)r %= m; if (r<0)r += m; return r; }
+template <typename T, typename S>T expo(T e, S n) { T x = 1, p = e; while (n) { if (n & 1)x = x*p; p = p*p; n >>= 1; }return x; }
+template <typename T>T power(T e, T n, T & m) { T x = 1, p = e; while (n) { if (n & 1)x = mod(x*p, m); p = mod(p*p, m); n >>= 1; }return x; }
+template <typename T>T powerL(T e, T n, T & m) { T x = 1, p = e; while (n) { if (n & 1)x = mulmod(x, p, m); p = mulmod(p, p, m); n >>= 1; }return x; }
+template <typename T> T InverseEuler(T a, T & m) { return (a == 1 ? 1 : power(a, m - 2, m)); }
+
+inline int two(int n) { return 1 << n; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+/*----------------------------------------------------------------------*/
+
+const int N = 1005;
+int dp[N][N];
+int arr[N],n,sum;
+
+int max_sum(int l, int r){
+	if (l>r)
+	{
+		return 0;
+	}
+
+	if (l == r)
+	{
+		return arr[l];
+	}
+
+	if (dp[l][r] != -1)
+	{
+		return dp[l][r];
+	}
+
+	int res = arr[l];
+	if (arr[l+1] >= arr[r])
+	{
+		res += max_sum(l+2, r);
+	} else {
+		res += max_sum(l+1, r-1);
+	}
+
+	dp[l][r] = res;
+	res = arr[r];
+
+	if (arr[l] >= arr[r-1])
+	{
+		res += max_sum(l+1, r-1);
+	} else {
+		res += max_sum(l, r-2);
+	}
+	dp[l][r] = max(dp[l][r], res);
+	return dp[l][r];
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+	
+	int t=1;
+    while(cin>>n && n){
+    	sum = 0;
+    	F(i, 0, n){
+    		cin>>arr[i];
+    		sum += arr[i];
+    	}
+
+    	memset(dp,-1, sizeof(dp));
+    	int ans = max_sum(0,n-1);
+    	cout<<"In game "<<t++<<", the greedy strategy might lose by as many as "<<2*ans-sum<<" points."<<endl;
+    }
+
+    return 0;          
+}/*
+    
+*/
+ 
---
+++ uva/Chapter 6/10298.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#define ABS(a) ((a < 0) ? ((-1)*(a)) : (a))
+#define F(i,a,b) for(long long i = (long long)(a); i < (long long)(b); i++)
+#define RF(i,a,b) for(long long i = (long long)(a); i >= (long long)(b); i--)
+#define MIN3(a,b,c) (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c))
+#define MAX(a,b) (a)>(b)?(a):(b)
+#define MIN2(a,b) (a)<(b)?(a):(b)
+#define SIEVELIM 10000000+10
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+using namespace std;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef long long ll;
+#define ull unsigned long long
+typedef long double ld;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+inline int nextint(){ int temporary; scanf("%d",&temporary); return temporary; }
+inline ll nextll(){ ll temporary; scanf("%lld",&temporary); return temporary; }
+inline double nextdouble(){double temporary; scanf("%lf",&temporary); return temporary;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll leftChild(ll p ){return p<<1;}
+ll rightChild(ll p ){return (p<<1)+1;}
+inline ll mid(ll l, ll r){ return (l+r)/2;}
+const ll MOD = 1000000007;
+const ll INF = 1e9+5;
+const double eps = 1e-7;
+const double PI = acos(-1.0);
+#define deb(x )     cerr << #x << " here "<< x; 
+#define endl    "\n"
+#define pb push_back
+#define mp make_pair
+#define all(cc) (cc).begin(),(cc).end()
+
+inline bool is_palindrome(const string& s){ return std::equal(s.begin(), s.end(), s.rbegin()); }
+
+template<typename T > void modulize(T & a, const T & b) { if (a >= b) { a %= b; } }
+ll mulmod(ll a, ll b, ll m) { ll q = (ll)(((ld)a*(ld)b) / (ld)m); ll r = a*b - q*m; if (r>m)r %= m; if (r<0)r += m; return r; }
+template <typename T, typename S>T expo(T e, S n) { T x = 1, p = e; while (n) { if (n & 1)x = x*p; p = p*p; n >>= 1; }return x; }
+template <typename T>T power(T e, T n, T & m) { T x = 1, p = e; while (n) { if (n & 1)x = mod(x*p, m); p = mod(p*p, m); n >>= 1; }return x; }
+template <typename T>T powerL(T e, T n, T & m) { T x = 1, p = e; while (n) { if (n & 1)x = mulmod(x, p, m); p = mulmod(p, p, m); n >>= 1; }return x; }
+template <typename T> T InverseEuler(T a, T & m) { return (a == 1 ? 1 : power(a, m - 2, m)); }
+
+inline int two(int n) { return 1 << n; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+/*----------------------------------------------------------------------*/
+
+const int N = 1e6+5;
+char t[N];
+int b[N],n;
+
+void preprocess() {
+	memset(b, 0, sizeof(b));
+	for (int i = 2; i <= n; i++){
+        int k = b[i - 1];
+        while (k > 0 && t[k] != t[i - 1])
+            k = b[k];
+        if (t[k] == t[i - 1])
+            b[i] = k + 1;
+        else
+            b[i] = 0;
+    }
+}
+
+int main(){
+    // std::ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+	
+    while(gets(t) && t[0]!='.') {
+    	n = strlen(t);
+    	preprocess();
+    	ll period = 0;
+    	
+    	F(i, 1, n+1){
+    		period = max(period, i-b[i]);
+    	}
+    	if (n % period)
+            period = n;
+        printf("%lld\n", n / period);
+    }	
+    return 0;          
+}/*
+    
+*/
+ 
